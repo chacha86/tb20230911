435ad138437d654476a50d44510416e0a8880710
chacha86 <taejincha0619@gmail.com>
2023-09-11 10:52:39 +0900

    텍게 요구사항2 - 게시물 등록 기능 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,26 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 이름 짓는 규칙
-        // 변수, 클래스 -> 명사
-        // 함수 -> 동사
-        // 단어 조합시 단어 의미가 바뀔 때마다 대문자 표현
-        //    -> 카멜 표기법
-        // 클래스 이름 지을 때는 첫글자 무조건 대문자.
-        // 누가 봐도. 추측 가능한. 단어 조합으로 지으세요.
-        // 파파고나 구글 번역기, 챗gpt 켜서
-        // 이름 짓는 건 너무너무너무너무너무너무너무 중요함
-
-        // 코드 정렬 (심심할 때마다 눌러주세요_
-        // Alt + Ctrl + L
 
         Scanner scan = new Scanner(System.in);
 
         while (true) {
+            System.out.print("명령어 : ");
             String command = scan.nextLine();
             if(command.equals("exit")) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
+            } else if(command.equals("add")) {
+                System.out.print("게시물 제목을 입력해주세요 : ");
+                String title = scan.nextLine();
+                System.out.print("게시물 내용을 입력해주세요 : ");
+                String content = scan.nextLine();
+
+                System.out.println("게시물이 등록되었습니다.");
             }
         }
     }
