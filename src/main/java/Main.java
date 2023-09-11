545b3faeba5501407db73fd8d0e40abd97dac875
chacha86 <taejincha0619@gmail.com>
2023-09-11 12:19:24 +0900

    텍게 요구사항3 - 게시물 목록 조회1(하나만 조회)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        String title = "";
+        String content = "";
 
         while (true) {
             System.out.print("명령어 : ");
@@ -13,11 +15,17 @@ public class Main {
                 break;
             } else if(command.equals("add")) {
                 System.out.print("게시물 제목을 입력해주세요 : ");
-                String title = scan.nextLine();
+                title = scan.nextLine();
                 System.out.print("게시물 내용을 입력해주세요 : ");
-                String content = scan.nextLine();
+                content = scan.nextLine();
 
                 System.out.println("게시물이 등록되었습니다.");
+            } else if(command.equals("list")) {
+
+                System.out.println("==================");
+                System.out.printf("제목 : %s\n", title);
+                System.out.printf("내용 : %s\n", content);
+                System.out.println("==================");
             }
         }
     }
