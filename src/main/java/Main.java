af6967c33353e518e88cb9af24eab44d6941ad9c
chacha86 <taejincha0619@gmail.com>
2023-09-11 10:27:00 +0900

    요구사항1 - 종료 기능 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,28 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // 이름 짓는 규칙
+        // 변수, 클래스 -> 명사
+        // 함수 -> 동사
+        // 단어 조합시 단어 의미가 바뀔 때마다 대문자 표현
+        //    -> 카멜 표기법
+        // 클래스 이름 지을 때는 첫글자 무조건 대문자.
+        // 누가 봐도. 추측 가능한. 단어 조합으로 지으세요.
+        // 파파고나 구글 번역기, 챗gpt 켜서
+        // 이름 짓는 건 너무너무너무너무너무너무너무 중요함
 
-        while(true) {
-            System.out.println("메뉴 입력 : ");
-            Scanner scan = new Scanner(System.in);
+        // 코드 정렬 (심심할 때마다 눌러주세요_
+        // Alt + Ctrl + L
 
-            String func = scan.nextLine();
+        Scanner scan = new Scanner(System.in);
 
-            if(func.equals("a")) {
-                System.out.println("기능1");
-            } else if(func.equals("b")) {
-                System.out.println("기능2");
-            } else if(func.equals("도움말")) {
-                System.out.println("a : 기능1");
-                System.out.println("b : 기능2");
+        while (true) {
+            String command = scan.nextLine();
+            if(command.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
             }
         }
-
-
-//        // 숫자 비교
-//        int a = 10;
-//        System.out.println(a == 10);
-//
-//        // 문자열 비교
-//        String str = "hello";
-//        System.out.println(str.equals("hello"));
-
-
     }
 }
