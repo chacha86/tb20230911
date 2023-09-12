02d018ee5977364e4b4578fac88ca0eb0d9c5ef9
chacha86 <taejincha0619@gmail.com>
2023-09-12 10:44:39 +0900

    텍게 요구사항5 - 게시물 삭제 기능 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,10 +12,10 @@ public class Main {
         while (true) {
             System.out.print("명령어 : ");
             String command = scan.nextLine();
-            if(command.equals("exit")) {
+            if (command.equals("exit")) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
-            } else if(command.equals("add")) {
+            } else if (command.equals("add")) {
                 System.out.print("게시물 제목을 입력해주세요 : ");
                 String title = scan.nextLine();
                 System.out.print("게시물 내용을 입력해주세요 : ");
@@ -25,9 +25,9 @@ public class Main {
                 articles.add(article);
 
                 System.out.println("게시물이 등록되었습니다.");
-            } else if(command.equals("list")) {
+            } else if (command.equals("list")) {
                 System.out.println("==================");
-                for(int i = 0; i < articles.size(); i++) {
+                for (int i = 0; i < articles.size(); i++) {
 
                     Article article = articles.get(i);
 
@@ -35,21 +35,42 @@ public class Main {
                     System.out.printf("제목 : %s\n", article.getTitle());
                     System.out.println("==================");
                 }
-            } else if(command.equals("update")) {
+            } else if (command.equals("update")) {
                 System.out.print("수정할 게시물 번호 : ");
                 int target = scan.nextInt();
 
                 scan.nextLine();
 
-                System.out.print("제목 : ");
-                String newTitle = scan.nextLine();
-                System.out.print("내용 : ");
-                String newContent = scan.nextLine();
+                if(0 <= target && target < articles.size()) {
+                    System.out.print("제목 : ");
+                    String newTitle = scan.nextLine();
+                    System.out.print("내용 : ");
+                    String newContent = scan.nextLine();
 
-                Article newArticle = new Article(newTitle, newContent);
-                articles.set(target - 1, newArticle);
+                    Article newArticle = new Article(newTitle, newContent);
+                    articles.set(target - 1, newArticle);
 
-                System.out.println("수정이 완료되었습니다.");
+                    System.out.println("수정이 완료되었습니다.");
+                } else {
+                    System.out.println("없는 게시물입니다.");
+                }
+
+            } else if (command.equals("delete")) {
+                System.out.print("삭제할 게시물 번호 : ");
+                int target = scan.nextInt();
+
+                scan.nextLine();
+
+                // index -> 0부터 시작하기 때문에 0보다 작으면 안됨
+                // 내가 가지고 있는 데이터의 개수 - 1 보다 크면 안됨
+                if(0 <= target && target < articles.size()) {
+                    articles.remove(target - 1);
+                    System.out.printf("%d번 게시물이 삭제되었습니다.\n", target);
+                } else {
+                    System.out.println("없는 게시물입니다.");
+                }
+
+
             }
         }
     }
